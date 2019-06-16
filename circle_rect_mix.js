61c4821ab5d973844840f27ef4ba5f619e0c517f
yujin12623 <yujin12623@gmail.com>
2019-06-16 14:54:12 +0900

    Beauty!

--- circle_rect_mix.js
+++ circle_rect_mix.js
@@ -12,20 +12,20 @@ function hsvToRGB(hue, saturation, value) {
     }
     hue = hue % 360;
 
-    saturation = saturation < 0 ? 0
-        : saturation > 1 ? 1
-        : saturation;
+    saturation = saturation < 0 ? 0 :
+        saturation > 1 ? 1 :
+        saturation;
 
-    value = value < 0 ? 0
-        : value > 1 ? 1
-        : value;
+    value = value < 0 ? 0 :
+        value > 1 ? 1 :
+        value;
 
     value *= 255;
-        hi = (hue / 60 | 0) % 6;
-        f = hue / 60 - hi;
-        p = value * (1 -           saturation) | 0;
-        q = value * (1 -      f  * saturation) | 0;
-        t = value * (1 - (1 - f) * saturation) | 0;
+    hi = (hue / 60 | 0) % 6;
+    f = hue / 60 - hi;
+    p = value * (1 - saturation) | 0;
+    q = value * (1 - f * saturation) | 0;
+    t = value * (1 - (1 - f) * saturation) | 0;
     value |= 0;
 
     switch (hi) {
@@ -48,92 +48,96 @@ function hsvToRGB(hue, saturation, value) {
 /**------------------キャンバス定義------------------------------**/
 
 /*キャンバス横幅*/
-var width=300;
+var width = 300;
 /*キャンバス縦幅*/
-var height=300;
-var canvas=document.createElement("canvas");
+var height = 300;
+var canvas = document.createElement("canvas");
 /*カラーピッカー用キャンバス*/
-var ctx=canvas.getContext("2d")
+var ctx = canvas.getContext("2d");
 
-canvas.width=width;
-canvas.height=height;
+canvas.width = width;
+canvas.height = height;
 document.body.appendChild(canvas);
 
 /**------------------カラーサークル描画------------------------------**/
 
 /*円の外側距離*/
-var routside=Math.min(canvas.width,canvas.height)/2
+var routside = Math.min(canvas.width, canvas.height) / 2;
 /*円の幅*/
-var widthofround=30
+var widthofround = 30;
 /*円の内側距離*/
-var rinside=routside-widthofround;
+var rinside = routside - widthofround;
 /*キャンバス中心X点*/
-var centerX=canvas.width/2;
+var centerX = canvas.width / 2;
 /*キャンバス中心Y点*/
-var centerY=canvas.height/2;
+var centerY = canvas.height / 2;
 
 ctx.beginPath();
 ctx.arc(centerX, centerY, routside, 0, Math.PI * 2, true);
 ctx.arc(centerX, centerY, rinside, 0, Math.PI * 2, false);
-ctx.fillStyle="#3912ff"
+ctx.fillStyle = "#3912ff";
 ctx.fill();
 
-var canvasdata=ctx.getImageData(0,0,width,height)
-var pixeldata=canvasdata.data;
+var canvasdata = ctx.getImageData(0, 0, width, height);
+var pixeldata = canvasdata.data;
 
-for(var x=0;x<canvas.width;x++){
-    for(var y=0;y<canvas.height;y++){
-        var basedindex=(y*width+x)*4;
-        
-        if(pixeldata[basedindex]>0){
-            var hue=Math.atan2(y-centerY,x-centerX)/Math.PI/2*360;
-            var colors=hsvToRGB(hue,100,100)
-            
-            pixeldata[basedindex]=colors[0]
-            pixeldata[basedindex+1]=colors[1]
-            pixeldata[basedindex+2]=colors[2]
-            
+for (x = 0; x < canvas.width; x++) {
+    for (y = 0; y < canvas.height; y++) {
+        var basedindex = (y * width + x) * 4;
+
+        if (pixeldata[basedindex] > 0) {
+            var hue = Math.atan2(y - centerY, x - centerX) / Math.PI / 2 * 360;
+            var colors = hsvToRGB(hue, 100, 100);
+
+            pixeldata[basedindex] = colors[0];
+            pixeldata[basedindex + 1] = colors[1];
+            pixeldata[basedindex + 2] = colors[2];
+
         }
     }
 }
-ctx.putImageData(canvasdata,0,0)
+ctx.putImageData(canvasdata, 0, 0);
 
 /**------------------カラースクエア描画------------------------------**/
 
 /*カラースクエア縦横長さ */
-var cwhlength=160
+var cwhlength = 160;
 /*カラースクエア開始X点 */
-var swp=(width/2)-(cwhlength/2);
+var swp = (width / 2) - (cwhlength / 2);
 /*カラースクエア開始Y点 */
-var shp=(height/2)-(cwhlength/2);
+var shp = (height / 2) - (cwhlength / 2);
 
-Color1=[255,255,255]
-Color2=hsvToRGB(50,100,100)
-var lr=Color1[0],lg=Color1[1],lb=Color1[2];
-var rr=Color2[0],rg=Color2[1],rb=Color2[2];
-var stepl=255/cwhlength;
-var steprr=rr/cwhlength;
-var steprg=rg/cwhlength;
-var steprb=rb/cwhlength;
+Color1 = [255, 255, 255];
+Color2 = hsvToRGB(50, 100, 100);
+var lr = Color1[0],
+    lg = Color1[1],
+    lb = Color1[2];
+var rr = Color2[0],
+    rg = Color2[1],
+    rb = Color2[2];
+var stepl = 255 / cwhlength;
+var steprr = rr / cwhlength;
+var steprg = rg / cwhlength;
+var steprb = rb / cwhlength;
 
-for(var startyh=shp;startyh<=shp+cwhlength;startyh++){
+for (startyh = shp; startyh <= shp + cwhlength; startyh++) {
     ctx.beginPath();
-    
-    var grad= ctx.createLinearGradient(swp,startyh,swp+cwhlength,startyh);
-  /* グラデーション終点のオフセットと色をセット */
-  grad.addColorStop(0,'rgb('+lr+','+lg+' , '+lb+')');
-  grad.addColorStop(1,'rgb('+rr+','+rg+' , '+rb+')');
-  ctx.fillStyle = grad;
-  ctx.rect(swp,startyh,cwhlength,1)
-  ctx.fill();
-        
-    lr-=stepl;
-    lg-=stepl;
-    lb-=stepl;
-    rr-=steprr;
-    rg-=steprg;
-    rb-=steprb;
-    
+
+    var grad = ctx.createLinearGradient(swp, startyh, swp + cwhlength, startyh);
+    /* グラデーション終点のオフセットと色をセット */
+    grad.addColorStop(0, 'rgb(' + lr + ',' + lg + ' , ' + lb + ')');
+    grad.addColorStop(1, 'rgb(' + rr + ',' + rg + ' , ' + rb + ')');
+    ctx.fillStyle = grad;
+    ctx.rect(swp, startyh, cwhlength, 1);
+    ctx.fill();
+
+    lr -= stepl;
+    lg -= stepl;
+    lb -= stepl;
+    rr -= steprr;
+    rg -= steprg;
+    rb -= steprb;
+
 }
 
 /**------------------カラーサークルピックポイント描画------------------------------**/
@@ -141,51 +145,48 @@ for(var startyh=shp;startyh<=shp+cwhlength;startyh++){
 /*マウスからの入力も別関数で並行*/
 
 /*サークルの幅の中心距離*/
-var rmiddle=routside-(widthofround/2);
+var rmiddle = routside - (widthofround / 2);
 /*マウス座標のX（仮値）マウス入力設置時は場所変更　毎入力ごとに初期化されてしまう*/
-var mx=100;
+var mx = 100;
 /*マウス座標のY（仮値）マウス入力設置時は場所変更　毎入力ごとに初期化されてしまう*/
-var my=200;
+var my = 200;
 /*セレクトポインタの角度*/
-var pointangle=Math.atan2(my-centerY,mx-centerX)/Math.PI/2*360;
+var pointangle = Math.atan2(my - centerY, mx - centerX) / Math.PI / 2 * 360;
 
-var SpointX=centerX+rmiddle*Math.cos(pointangle*(Math.PI/180))
-var SpointY=centerY+rmiddle*Math.sin(pointangle*(Math.PI/180))
+var SpointX = centerX + rmiddle * Math.cos(pointangle * (Math.PI / 180));
+var SpointY = centerY + rmiddle * Math.sin(pointangle * (Math.PI / 180));
 
-Spointpaint(SpointX,SpointY);
+Spointpaint(SpointX, SpointY);
 
 /**------------------カラースクエアピックポイント描画------------------------------**/
 /*ファンクション化時に実行呼び出し初期化関数にデフォルト位置のXYを渡させる*/
 /*マウスからの入力も別関数で並行*/
 
 /*マウス座標のX（仮値）*/
-var msx=7110
+var msx = 7110;
 /*マウス座標のY（仮値）*/
-var msy=2120
+var msy = 2120;
 /*ポインタ表示位置X　　マウス入力設置時は場所変更　毎入力ごとに初期化されてしまう*/
-var SSpointX=swp+cwhlength;
+var SSpointX = swp + cwhlength;
 /*ポインタ表示位置Y　　マウス入力設置時は場所変更　毎入力ごとに初期化されてしまう*/
-var SSpointY=shp;  
-console.log(canvas.offsetTop)
-if(msx>=canvas.offsetLeft+swp&&msx<=canvas.offsetLeft+swp+cwhlength){
-    SSpointX=msx-canvas.offsetLeft;
-    console.log("D")
+var SSpointY = shp;
+
+if (msx >= canvas.offsetLeft + swp && msx <= canvas.offsetLeft + swp + cwhlength) {
+    SSpointX = msx - canvas.offsetLeft;
 }
-if(msy>=canvas.offsetTop+shp&&msy<=canvas.offsetTop+shp+cwhlength){
-        
-        SSpointY=msy-canvas.offsetTop;
+if (msy >= canvas.offsetTop + shp && msy <= canvas.offsetTop + shp + cwhlength) {
+    SSpointY = msy - canvas.offsetTop;
 }
-Spointpaint(SSpointX,SSpointY);
+Spointpaint(SSpointX, SSpointY);
 
 
-
-function Spointpaint(PX,PY){
-    console.log(PX,PY)
+function Spointpaint(PX, PY) {
+    console.log(PX, PY);
     ctx.beginPath();
     ctx.arc(PX, PY, 7, 0, Math.PI * 2, true);
     ctx.arc(PX, PY, 4, 0, Math.PI * 2, false);
-    ctx.strokeStyle="#000000"
-    ctx.stroke()
-    ctx.fillStyle="#ffffff"
+    ctx.strokeStyle = "#000000";
+    ctx.stroke();
+    ctx.fillStyle = "#ffffff";
     ctx.fill();
 }
